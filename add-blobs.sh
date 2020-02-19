41663faa9be00896a006f72d1b82a61e776879ec
Nguyen Duy Tho <nguyenduytho@gmail.com>
2020-02-19 16:48:32 -0500

    release 1.0

--- add-blobs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-wget https://amazon-ecr-credential-helper-releases.s3.us-east-2.amazonaws.com/0.4.0/linux-amd64/docker-credential-ecr-login
-bosh add-blob docker-credential-ecr-login ecr-credential-helper/docker-credential-ecr-login
-
